de8aeb7d0a1f41869067ad93cc42392133da280e
Francis Henriquez Matos <francishenriquez07@gmail.com>
2023-03-07 01:08:14 -0400

    api request

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,13 @@ class AppState extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => MoviesProvider())],
-      child: MyApp(),
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MoviesProvider(),
+          lazy: false,
+        ),
+      ],
+      child: const MyApp(),
     );
   }
 }
--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,13 +1,28 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier {
+  final String _apiKey = 'afe3b088648519f7ebbc0b79f56e70e3';
+  final String _baseUrl = 'api.themoviedb.org';
+  final String _language = 'es-US';
+
   MoviesProvider() {
     print('MoviesProvider inicializado');
 
     getOnDisplayMovies();
   }
 
-  getOnDisplayMovies() {
-    print('getOnDisplayMovies');
+  getOnDisplayMovies() async {
+    var url = Uri.https(_baseUrl, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+    // Await the http get response, then decode the json-formatted response.
+    final response = await http.get(url);
+
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+
+    print(decodedData['dates']);
   }
 }
